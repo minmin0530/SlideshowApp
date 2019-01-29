22616857f39c46bdc5f4f9f3a4c47712e8b9a2c0
izumi_yoshiki <izumiyoshiki@icloud.com>
2019-01-29 20:28:30 +0900

    自動送り、文言、戻る、AutoLayout、画像、の修正

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -21,10 +21,19 @@ class ViewController: UIViewController {
     }
         
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if timer != nil {
+            timer.invalidate()
+            timer = nil
+        }
         let resultViewController:ResultViewController = segue.destination as! ResultViewController
         resultViewController.image = imageView.image
     }
     @IBAction func unwind(_ segue: UIStoryboardSegue) {
+        if playAndStopButton.titleLabel?.text == "停止" {
+            timer = Timer.scheduledTimer(withTimeInterval: 2, repeats: true, block: { (Timer) in
+                self.nextImage()
+            })
+        }
     }
     func nextImage() {
         imageNumber += 1
@@ -33,9 +42,11 @@ class ViewController: UIViewController {
         }
         imageView.image = images[imageNumber]
     }
+    @IBOutlet weak var nextButton: UIButton!
     @IBAction func nextButton(_ sender: Any) {
         nextImage()
     }
+    @IBOutlet weak var backButton: UIButton!
     @IBAction func backButton(_ sender: Any) {
         imageNumber -= 1
         if imageNumber < 0 {
@@ -43,14 +54,21 @@ class ViewController: UIViewController {
         }
         imageView.image = images[imageNumber]
     }
+    @IBOutlet weak var playAndStopButton: UIButton!
     @IBAction func playAndStopButton(_ sender: Any) {
         if timer == nil {
+            playAndStopButton.setTitle("停止", for: .normal)
+            nextButton.isEnabled = false
+            backButton.isEnabled = false
             timer = Timer.scheduledTimer(withTimeInterval: 2, repeats: true, block: { (Timer) in
                 self.nextImage()
             })
         } else {
             timer.invalidate()
             timer = nil
+            playAndStopButton.setTitle("再生", for: .normal)
+            nextButton.isEnabled = true
+            backButton.isEnabled = true
         }
     }
 }
